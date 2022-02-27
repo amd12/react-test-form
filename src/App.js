340b494850c58a03257618d2761678378400bcd2
amd12 <tyr.artem@gmail.com>
2022-02-27 18:26:55 +0300

    Add API request, loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, {useEffect, useState} from "react";
 import './style/app.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,19 +6,32 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/myModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'aa', body: 'description javascript'},
-        {id: 2, title: 'bb', body: 'description java'},
-        {id: 3, title: 'vv', body: 'description java'}
-    ]);
-
+    const [posts, setPosts] = useState([] );
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
-
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.sort);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    async function fetchPost(){
+        setIsLoading(true);
+        setTimeout(async ()=>{
+
+            const posts = await PostService.getAll();
+            setPosts(posts.data);
+            setIsLoading(false);
+
+        },3000)
+    }
+
+    useEffect(()=>{
+        fetchPost()
+    },[])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -30,12 +43,16 @@ function App() {
 
     return (
         <div className="App">
+            <button onClick={fetchPost}>Get request</button>
             <MyButton style={{marginTop: 30}} onClick={()=>setModal(true)}>
                 Popup
             </MyButton>
             <MyModal visible={modal} setVisible={setModal}><PostForm create={createPost}/></MyModal>
             <hr style={{margin: '20px'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
+            {
+                isLoading && <div className='content'><Loader/></div>
+            }
             <PostList remove={deletePost} posts={sortedAndSearchedPosts}/>
         </div>
     );
